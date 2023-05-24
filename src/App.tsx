ce5c8e146142daa85d925ad1998f43f724694613
shahana <rafiyashahana@gmail.com>
2023-05-24 16:56:50 +0300

    initial router setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Search from "./pages/Search";
+import Watch from "./pages/Watch";
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/watch/:id" element={<Watch />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
